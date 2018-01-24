31e502d9c676ee1c8ac3de6aaf786b6ac386df76
Billy Wu <lake_ice@hotmail.com>
2018-01-24 14:38:40 +0800

    clean up code

--- LearnSocketAsyncEventArgs/SocketAsyncServer/SocketListenerSettings.cs
+++ LearnSocketAsyncEventArgs/SocketAsyncServer/SocketListenerSettings.cs
@@ -1,115 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net;
-using System.Text;
+﻿using System.Net;
 
 namespace SocketAsyncServer
 {
-    class SocketListenerSettings
+    internal class SocketListenerSettings
     {
         // the maximum number of connections the sample is designed to handle simultaneously 
-        private Int32 maxConnections;
+
 
         // this variable allows us to create some extra SAEA objects for the pool,
         // if we wish.
-        private Int32 numberOfSaeaForRecSend;
-
-        // max # of pending connections the listener can hold in queue
-        private Int32 backlog;
-
-        // tells us how many objects to put in pool for accept operations
-        private Int32 maxSimultaneousAcceptOps;
-
-        // buffer size to use for each socket receive operation
-        private Int32 receiveBufferSize;
-
-        // length of message prefix for receive ops
-        private Int32 receivePrefixLength;
-
-        // length of message prefix for send ops
-        private Int32 sendPrefixLength;
 
         // See comments in buffer manager.
-        private Int32 opsToPreAllocate;
 
+
+        public SocketListenerSettings(int maxConnections, int excessSaeaObjectsInPool, int backlog,
+            int maxSimultaneousAcceptOps, int receivePrefixLength, int receiveBufferSize, int sendPrefixLength,
+            int opsToPreAlloc, IPEndPoint theLocalEndPoint)
+        {
+            MaxConnections = maxConnections;
+            NumberOfSaeaForRecSend = maxConnections + excessSaeaObjectsInPool;
+            Backlog = backlog;
+            MaxAcceptOps = maxSimultaneousAcceptOps;
+            ReceivePrefixLength = receivePrefixLength;
+            BufferSize = receiveBufferSize;
+            SendPrefixLength = sendPrefixLength;
+            OpsToPreAllocate = opsToPreAlloc;
+            LocalEndPoint = theLocalEndPoint;
+        }
+        // max # of pending connections the listener can hold in queue
+        public int MaxConnections { get; }
+        public int NumberOfSaeaForRecSend { get; }
+
+        public int Backlog { get; }
+
+        // tells us how many objects to put in pool for accept operations
+        public int MaxAcceptOps { get; }
+
+        // length of message prefix for receive ops
+        public int ReceivePrefixLength { get; }
+
+        // buffer size to use for each socket receive operation
+        public int BufferSize { get; }
+
+        // length of message prefix for send ops
+        public int SendPrefixLength { get; }
+
+        public int OpsToPreAllocate { get; }
         // Endpoint for the listener.
-        private IPEndPoint localEndPoint;
-
-        public SocketListenerSettings(Int32 maxConnections, Int32 excessSaeaObjectsInPool, Int32 backlog, Int32 maxSimultaneousAcceptOps, Int32 receivePrefixLength, Int32 receiveBufferSize, Int32 sendPrefixLength, Int32 opsToPreAlloc, IPEndPoint theLocalEndPoint)
-        {
-            this.maxConnections = maxConnections;
-            this.numberOfSaeaForRecSend = maxConnections + excessSaeaObjectsInPool;
-            this.backlog = backlog;
-            this.maxSimultaneousAcceptOps = maxSimultaneousAcceptOps;
-            this.receivePrefixLength = receivePrefixLength;
-            this.receiveBufferSize = receiveBufferSize;
-            this.sendPrefixLength = sendPrefixLength;
-            this.opsToPreAllocate = opsToPreAlloc;
-            this.localEndPoint = theLocalEndPoint;
-        }
-
-        public Int32 MaxConnections
-        {
-            get
-            {
-                return this.maxConnections;
-            }
-        }
-        public Int32 NumberOfSaeaForRecSend
-        {
-            get
-            {
-                return this.numberOfSaeaForRecSend;
-            }
-        }
-        public Int32 Backlog
-        {
-            get
-            {
-                return this.backlog;
-            }
-        }
-        public Int32 MaxAcceptOps
-        {
-            get
-            {
-                return this.maxSimultaneousAcceptOps;
-            }
-        }
-        public Int32 ReceivePrefixLength
-        {
-            get
-            {
-                return this.receivePrefixLength;
-            }
-        }
-        public Int32 BufferSize
-        {
-            get
-            {
-                return this.receiveBufferSize;
-            }
-        }
-        public Int32 SendPrefixLength
-        {
-            get
-            {
-                return this.sendPrefixLength;
-            }
-        }
-        public Int32 OpsToPreAllocate
-        {
-            get
-            {
-                return this.opsToPreAllocate;
-            }
-        }
-        public IPEndPoint LocalEndPoint
-        {
-            get
-            {
-                return this.localEndPoint;
-            }
-        }    
-    }    
+        public IPEndPoint LocalEndPoint { get; }
+    }
 }
